1207d0922dd4809e88159d3d5feadafb293b6069
manish136 <manishvaleti@gmail.com>
2022-04-18 19:47:45 +0530

    18-04(1) commit

--- cargo_vault/cargo_vault/settings.py
+++ cargo_vault/cargo_vault/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-3kx-+g)6wx7cnl#ygaw)1kbb*v%ajwas-eve4%g8kgw#tp#2!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
