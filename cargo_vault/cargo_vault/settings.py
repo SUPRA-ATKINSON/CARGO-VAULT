c01b2f6373c42d614298884bac6088b00b43e562
manish136 <manishvaleti@gmail.com>
2022-04-18 18:56:14 +0530

    Zero Commit

--- cargo_vault/cargo_vault/settings.py
+++ cargo_vault/cargo_vault/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3kx-+g)6wx7cnl#ygaw)1kbb*v%ajwas-eve4%g8kgw#tp#2!*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
